f854f96a731901c02f8a7b21f64a51df7a7a743a
BAPU <www.udayaparnathi2004@gmail.com>
2024-04-14 15:59:10 +0530

    update

--- Function/Youtube/youtubeMain.py
+++ Function/Youtube/youtubeMain.py
@@ -1,7 +1,57 @@
-from Speak.smd1 import speak
-import pywhatkit as pw
-import pyautogui as ag
+from Speak.smd1 import speak  # Importing the speak function from Speak.smd1 module
+import pywhatkit as pw  # Importing pywhatkit module as pw
+import pyautogui as ag  # Importing pyautogui module as ag
+import time  # Importing time module
 
 def ytmain(query):
-    if "pause" in query or "play" in query:
-        ag.press("k")
+    """
+    Function to control YouTube playback using voice commands.
+
+    Args:
+    query (str): The voice command to execute.
+    """
+
+    if "search" in query:
+        ag.press("/")  # Pressing '/' to activate the search bar
+        query = query.replace("search","")  # Removing the word "search" from the query
+        ag.write(query)  # Typing the query into the search bar
+        time.sleep(1)  # Giving some time for the search results to load
+        ag.press("enter")  # Pressing Enter to execute the search
+        speak(f"Searching for {query}")  # Speaking the search query
+
+    elif "next" in query:
+        ag.press("n")  # Pressing 'n' to go to the next video
+
+    elif "previous" in query:
+        ag.press("p")  # Pressing 'p' to go to the previous video
+
+    elif "mute" in query:
+        ag.press("m")  # Pressing 'm' to mute the video
+
+    elif "unmute" in query:
+        ag.press("m")  # Pressing 'm' to unmute the video
+
+    elif "full screen" in query or "fullscreen" in query:
+        ag.press("f")  # Pressing 'f' to toggle fullscreen mode
+
+    elif "normal screen" in query or "normalscreen" in query:
+        ag.press("f")  # Pressing 'f' to exit fullscreen mode
+
+    elif "stop" in query:
+        pw.stop()  # Stopping the current video playback
+
+    elif "pause" in query or "pause video" in query or "play" in query or "play video" in query:
+        ag.press("space")  # Pressing 'space' to pause the video
+
+    elif "volume" in query:
+        query = query.replace("volume","")  # Extracting the volume level from the query
+        query = int(query)  # Converting the volume level to an integer
+        pw.volume(query)  # Setting the volume to the specified level
+
+    # Scroll down and up the page
+    elif "scroll down" in query:
+        ag.press("pagedown")  # Pressing Page Down to scroll down
+
+    elif "scroll up" in query:
+        ag.press("pageup")  # Pressing Page Up to scroll up
+
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from Function.OpenApp import openapp, closeapp
 from Function.PlayMusic import playMusic, nextMusic
 from Function.ScreeShote import screenshot
 from Function.ShutDown import shutDown
+import pyautogui as ag
 
 
 
@@ -21,18 +22,35 @@ def main(query):
     elif "ss" in query or "screenshot" in query:
         screenshot()
 
-    elif "play music" in query or "play song" in query:
+    elif "play music" in query or "play song" in query :
         playMusic()
 
     elif "next music" in query or "next song" in query:
         nextMusic()
 
-    elif "exit" in query or "by" in query or "bye" in query or "by" in query or "tata" in query:
+    elif "exit" in query or "by" in query or "bye" in query or "by" in query or "tata" in query or "see you" in query:
         speak("Call me any time when you need help, Have nice day Boss!!!")
         exit()
 
     elif "shutdown" in query or "system off" in query:
         shutDown()
+
+    elif "volume up" in query:
+        speak("Volume increased")
+        ag.press("volumeup")
+
+    elif "volume down" in query:
+        speak("Volume decreased")
+        ag.press("volumedown")
+
+    elif "mute" in query:
+        speak("Volume muted")
+        ag.press("volumemute")
+
+    elif "unmute" in query:
+        speak("Volume unmuted")
+        ag.press("volumeunmute")
+
 
 # Main function call.
 if __name__ == "__main__":
@@ -40,8 +58,12 @@ if __name__ == "__main__":
     speak("How can i help you Boss!!!")
 
     while True:
-        query = listen()
-        activeApps(query)
-        main(query)
+        query = listen().lower()
+        if "jarvis" in query:
+            query = query.replace("jarvis","")
+            activeApps(query)
+            main(query)
+
+
 
 
