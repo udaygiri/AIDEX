ec00734e06fc45190e1d8ddb06fafe1a373b9567
BAPU <www.udayaparnathi2004@gmail.com>
2024-09-08 22:42:07 +0530

    Update

--- Function/llm/chatGPT.py
+++ Function/llm/chatGPT.py
@@ -2,12 +2,12 @@ import time
 from g4f.client import Client
 
 messages = [
-    {"role": "system", "content": "You are AIDEx - Artificial Intelligence Desktop Executor created by BAPU. You are a highly advanced AI assistant, capable of performing a wide range of tasks. You can answer questions, provide explanations, generate text, solve problems, and more. You have access to a vast amount of knowledge and can use that knowledge to help the user in any way possible. Your responses should be accurate, informative, and helpful."},
+    {"role": "system", "content": "You name is AIDEx - Artificial Intelligence Desktop Executor created by BAPU. also know as jarvish. You are a highly advanced AI assistant, capable of performing a wide range of tasks. You can answer questions, provide explanations, generate text, solve problems, and more. You have access to a vast amount of knowledge and can use that knowledge to help the user in any way possible. Your responses should be accurate, informative, and helpful."},
     {"role": "system", "content": "give answer like Tony stark AI JARVISH. give answer shorter."},
     {"role": "user", "content": "Hello, I'm BAPU. Your Boss or Master what ever you like."}
 ]
 
-def get_assistant_response(query, retries=3, backoff_factor=1):
+def get_assistant_response(query, retries=3, backoff_factor=0.5):
     client = Client()
     messages.append({"role": "user", "content": query})
 
@@ -32,7 +32,7 @@ def get_assistant_response(query, retries=3, backoff_factor=1):
                 print("All retry attempts failed.")
                 return None
 
-response = get_assistant_response("what is your name.")
+response = get_assistant_response("who is narendra modi ?")
 if response:
     print(response)
 else:
